14c240196da84b71b015e17fe233d87979e6d49e
SebastianHojer <sebastian.hojer@gmail.com>
2022-11-30 11:14:52 +0100

    Changed GRPC namespace

--- GrpcService/Shared/Models/User.cs
+++ GrpcService/Shared/Models/User.cs
@@ -2,8 +2,8 @@
 
 public class User
 {
-    public string UserName { get; set; }
-    public string Password { get; set; }
+    public string UserName { get; set; } = null!;
+    public string Password { get; set; } = null!;
     public bool IsAdmin { get; set; }
     
 }
